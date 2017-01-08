6f8d02e19aa8eb8f1b2dcb8be8d965c6b0327542
Volodymyr Solodkyi <Volodymyr Solodkyi>
2017-01-08 20:26:37 +0200

    type: lesson2
    body
    
    footer

--- lesson_2.py
+++ lesson_2.py
@@ -1,5 +1,6 @@
 import sys
-x = int(sys.argv[1])
-y = int(sys.argv[2])
-
-print x + y
+from math import sqrt, exp
+a = float(sys.argv[1])
+b = float(sys.argv[2])
+x = sqrt(a*b)/(exp(a)*b)+a*exp(2*a/b)
+print (x)
